ac02c7d66a7e2d925a6a8e70adfa530ab614faa7
Huy Trinh Duc <huyparody@Nguyens-Mac-mini.local>
2018-08-21 17:56:46 +0700

    Update menu and lessons

---
+++ buoi1-login/Controllers/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  buoi1-login
+//
+//  Created by Huy Trinh Duc on 8/21/18.
+//  Copyright © 2018 Huy Trinh Duc. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
